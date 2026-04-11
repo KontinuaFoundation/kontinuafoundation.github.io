50c129e814780cf11131391f313d05d8c6f91cb1
Arjan Ellingson <67170142+asellingson28@users.noreply.github.com>
2026-04-11 00:56:25 -0700

    improvement to app.jsx, remove old pdfs

--- kontinua-site/src/App.jsx
+++ kontinua-site/src/App.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [workbooks, setWorkbooks] = useState([]);
+
+  useEffect(() => {
+    fetch("./workbooks.json")
+      .then((r) => r.json())
+      .then(setWorkbooks);
+  }, []);
+
   return (
     <main>
       <h1>Table of Contents for Digital Resources</h1>
-      <p>Test for react front end.</p>
+
+      {workbooks.map((wb) => (
+        <section key={wb.href}>
+          <h2>
+            <a href={wb.href}>{wb.title}</a>
+          </h2>
+          <ul>
+            {wb.chapters.map((ch) => (
+              <li key={ch.id}>
+                <a href={`${wb.href}#${ch.id}`}>
+                  {ch.chap_num}: {ch.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
     </main>
   );
 }
